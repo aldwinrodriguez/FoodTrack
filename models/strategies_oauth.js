2148bf15f3a40e9af4c0faeeb8bdf717bb2123c8
Aldwin Rodriguez <aldwin.rodriguez@owl.ucc.edu>
2019-05-11 14:57:47 -0400

    heroku

--- models/strategies_oauth.js
+++ models/strategies_oauth.js
@@ -14,7 +14,7 @@ strategies.local = account;
 strategies.google = passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "http://foodtrack2019.com/auth/google/callback"
     },
     function (accessToken, refreshToken, profile, cb) {
         strategies.local.findOne({
