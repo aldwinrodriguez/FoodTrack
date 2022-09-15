62e834e1b3ef11444bba614ea0dfe660dc5a785e
Aldwin <niwdla0405@gmail.com>
2022-09-15 18:09:58 -0400

    MongoDB update

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 
 const app = express();
 // app configurations
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(session({
-    secret: 'keyboard cat',
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 // mongoDB config
 
 mongoose.connect(process.env.CONNECTION, {
-    useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('we\'re connected!', db.port));
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("we're connected!", db.port));
 
 // import Strategies
-const Strategy = require(__dirname + '/models/strategies_oauth.js');
+const Strategy = require(__dirname + "/models/strategies_oauth.js");
 // Local Strategy
 passport.use(new LocalStrategy(Strategy.local.authenticate()));
 // Strategies
@@ -46,50 +51,50 @@ passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
 // Routes
-const routeCb = require(__dirname + '/route_cbs/route_callbacks.js');
+const routeCb = require(__dirname + "/route_cbs/route_callbacks.js");
 
-app.post('/react', (req, res) => {
-    res.send(req.body.items[2]);
-})
+app.post("/react", (req, res) => {
+  res.send(req.body.items[2]);
+});
 
 // home
-app.route('/')
-    .get(routeCb.home)
-    .post(routeCb.postHome);
+app.route("/").get(routeCb.home).post(routeCb.postHome);
 
 // login
-app.route('/login')
-    .get(routeCb.login)
-    .post(routeCb.postLogin);
+app.route("/login").get(routeCb.login).post(routeCb.postLogin);
 
 // register
-app.route('/register')
-    .get(routeCb.register)
-    .post(routeCb.postRegister);
+app.route("/register").get(routeCb.register).post(routeCb.postRegister);
 
 // logout
-app.get('/logout', routeCb.logout);
+app.get("/logout", routeCb.logout);
 
 // oauths
-const auth = require(__dirname + '/route_cbs/authenticate.js');
+const auth = require(__dirname + "/route_cbs/authenticate.js");
 
 // google request
-app.get('/auth/google', auth.google);
-app.get('/auth/google/callback', auth.googleCb, (req, res) => res.redirect('/'));
+app.get("/auth/google", auth.google);
+app.get("/auth/google/callback", auth.googleCb, (req, res) =>
+  res.redirect("/")
+);
 
 // facebook request
-app.get('/auth/facebook', auth.facebook);
-app.get('/auth/facebook/callback', auth.facebookCb, (req, res) => res.redirect('/'));
+app.get("/auth/facebook", auth.facebook);
+app.get("/auth/facebook/callback", auth.facebookCb, (req, res) =>
+  res.redirect("/")
+);
 
 // twitter request
-app.get('/auth/twitter', auth.twitter);
-app.get('/auth/twitter/callback', auth.twitterCb, (req, res) => res.redirect('/'));
+app.get("/auth/twitter", auth.twitter);
+app.get("/auth/twitter/callback", auth.twitterCb, (req, res) =>
+  res.redirect("/")
+);
 
 // temporary policy
-app.get('/policy', routeCb.policy);
+app.get("/policy", routeCb.policy);
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
+  port = 3000;
 }
 app.listen(port);
--- models/strategies_oauth.js
+++ models/strategies_oauth.js
@@ -1,108 +1,108 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const TwitterStrategy = require('passport-twitter').Strategy;
-const mongoose = require('mongoose');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const TwitterStrategy = require("passport-twitter").Strategy;
+const mongoose = require("mongoose");
 
-const account = require('../models/accounts.js')
+const account = require("../models/accounts.js");
 
 let strategies = {};
 
 strategies.local = account;
 
 // passport google
-strategies.google = passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://foodtrack2019.com/auth/google/callback"
-    },
-    function (accessToken, refreshToken, profile, cb) {
-        strategies.local.findOne({
-            username: profile.id
-        }, function (err, user) {
-            console.log(profile);
+// strategies.google = passport.use(new GoogleStrategy({
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://foodtrack2019.com/auth/google/callback"
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//         strategies.local.findOne({
+//             username: profile.id
+//         }, function (err, user) {
+//             console.log(profile);
 
-            if (err) {
-                return console.log(err);
-            }
-            if (!user) {
-                let account = new strategies.local({
-                    username: profile.id,
-                    name: profile.displayName,
-                    // update
-                    pro_pic: profile.photos[0].value,
-                    provider: profile.provider
-                });
-                account.save(err => {
-                    if (err) return console.log(err);
-                });
-                return cb(err, user);
-            }
-            return cb(err, user);
-        });
-    }
-));
+//             if (err) {
+//                 return console.log(err);
+//             }
+//             if (!user) {
+//                 let account = new strategies.local({
+//                     username: profile.id,
+//                     name: profile.displayName,
+//                     // update
+//                     pro_pic: profile.photos[0].value,
+//                     provider: profile.provider
+//                 });
+//                 account.save(err => {
+//                     if (err) return console.log(err);
+//                 });
+//                 return cb(err, user);
+//             }
+//             return cb(err, user);
+//         });
+//     }
+// ));
 
-strategies.facebook = passport.use(new FacebookStrategy({
-        clientID: process.env.FACEBOOK_APP_ID,
-        clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "https://foodtrack2019.com/auth/facebook/callback",
-        profileFields: ['id', 'emails', 'name', 'picture']
-    },
-    function (accessToken, refreshToken, profile, cb) {
-        strategies.local.findOne({
-            username: profile.id
-        }, function (err, user) {
-            if (err) {
-                return console.log("TCL: err", err)
-            }
-            if (!user) {
-                let account = new strategies.local({
-                    username: profile.id,
-                    name: profile.name.givenName + ' ' + profile.name.familyName,
-                    // update
-                    pro_pic: profile.photos[0].value,
-                    provider: profile.provider
-                });
-                account.save(err => {
-                    if (err) return console.log(err);
-                });
-                return cb(err, user);
-            }
-            return cb(err, user);
-        });
-    }
-));
+// strategies.facebook = passport.use(new FacebookStrategy({
+//         clientID: process.env.FACEBOOK_APP_ID,
+//         clientSecret: process.env.FACEBOOK_APP_SECRET,
+//         callbackURL: "https://foodtrack2019.com/auth/facebook/callback",
+//         profileFields: ['id', 'emails', 'name', 'picture']
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//         strategies.local.findOne({
+//             username: profile.id
+//         }, function (err, user) {
+//             if (err) {
+//                 return console.log("TCL: err", err)
+//             }
+//             if (!user) {
+//                 let account = new strategies.local({
+//                     username: profile.id,
+//                     name: profile.name.givenName + ' ' + profile.name.familyName,
+//                     // update
+//                     pro_pic: profile.photos[0].value,
+//                     provider: profile.provider
+//                 });
+//                 account.save(err => {
+//                     if (err) return console.log(err);
+//                 });
+//                 return cb(err, user);
+//             }
+//             return cb(err, user);
+//         });
+//     }
+// ));
 
-strategies.twitter = passport.use(new TwitterStrategy({
-        consumerKey: process.env.TWITTER_CONSUMER_KEY,
-        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-        callbackURL: "http://foodtrack2019.com/auth/twitter/callback"
-    },
-    function (token, tokenSecret, profile, cb) {
-        strategies.local.findOne({
-            username: profile.id
-        }, function (err, user) {
-            // console.log(profile);
-            if (err) {
-                return console.log(err);
-            }
-            if (!user) {
-                let account = new strategies.local({
-                    username: profile.id,
-                    name: profile.displayName,
-                    // update
-                    pro_pic: profile.photos[0].value,
-                    provider: profile.provider
-                });
-                account.save(err => {
-                    if (err) return console.log(err);
-                });
-                return cb(err, user);
-            }
-            return cb(err, user);
-        });
-    }
-));
+// strategies.twitter = passport.use(new TwitterStrategy({
+//         consumerKey: process.env.TWITTER_CONSUMER_KEY,
+//         consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+//         callbackURL: "http://foodtrack2019.com/auth/twitter/callback"
+//     },
+//     function (token, tokenSecret, profile, cb) {
+//         strategies.local.findOne({
+//             username: profile.id
+//         }, function (err, user) {
+//             // console.log(profile);
+//             if (err) {
+//                 return console.log(err);
+//             }
+//             if (!user) {
+//                 let account = new strategies.local({
+//                     username: profile.id,
+//                     name: profile.displayName,
+//                     // update
+//                     pro_pic: profile.photos[0].value,
+//                     provider: profile.provider
+//                 });
+//                 account.save(err => {
+//                     if (err) return console.log(err);
+//                 });
+//                 return cb(err, user);
+//             }
+//             return cb(err, user);
+//         });
+//     }
+// ));
 
 module.exports = strategies;
